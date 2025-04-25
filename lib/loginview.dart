cc7c9986a9d94eaf1077a7d359f3754b56b8d545
hasfarworkm <hasfarwork@gmail.com>
2025-04-25 18:34:10 +0500

    adding text field

---
+++ lib/loginview.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.blue, title: Text("Login Screen")),
+      body: Container(
+        width: 200,
+        height: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+          children: [
+            // Image.asset('assets/images/car2.jpg'),
+            //  Image.asset('assets/images/car rear.jpg'),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "User name",
+                hintText: "Enter your username",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.deepOrange, width: 3),
+                ),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Password",
+                hintText: "Enter your password",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.deepOrange, width: 3),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
