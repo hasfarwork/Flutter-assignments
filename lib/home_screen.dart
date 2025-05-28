5aa6a86ed61560622a19ace73ea716dbff072bd7
hasfar <hasfarwork@gmail.com>
2025-05-28 12:33:28 +0500

    Listview inside column-expanded-shrinkwrap-physics

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:firstflutter/about_screen.dart';
-import 'package:firstflutter/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,6 +6,7 @@ class HomeScreen extends StatelessWidget {
   List<Map> details = [
     {"name": "asif", "age": 27, "city": "lahore"},
     {"name": "saif", "age": 30, "city": "Karachi"},
+    {"name": "Huzaifa", "age": 30, "city": "Islamabad"},
   ];
   @override
   Widget build(BuildContext context) {
@@ -16,26 +16,36 @@ class HomeScreen extends StatelessWidget {
 
         title: Text("Home Screen", style: TextStyle(fontSize: 30)),
       ),
-      body: ListView.builder(
-        itemCount: details.length,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.only(bottom: 20),
-            child: ListTile(
-              title: Text(details[index]['name']),
-              tileColor: Colors.blueGrey,
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder:
-                        (context) => AboutScreen(studentData: details[index]),
+      body: Column(
+        children: [
+          Container(color: Colors.yellow, height: 300),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              // physics: NeverScrollableScrollPhysics(),
+              itemCount: 60,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  child: ListTile(
+                    title: Text("details[index]['name']"),
+                    tileColor: Colors.blueGrey,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder:
+                              (context) =>
+                                  AboutScreen(studentData: details[index]),
+                        ),
+                      );
+                    },
                   ),
                 );
               },
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
